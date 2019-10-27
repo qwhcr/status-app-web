0081fbddf5d8ba565b1cf076d436bb17fa5f07a0
QiusenHuangGT <qiusenhuang17@hotmail.com>
2019-10-27 00:13:36 -0400

    adding build

---
+++ build/precache-manifest.4df3492433206d850e539a1f652a3eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8a8d8b596656de0ba6ff692e22a61c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5de23796ca1b0156145d",
+    "url": "/static/css/2.536f1d1e.chunk.css"
+  },
+  {
+    "revision": "356467dd2cb35a513520",
+    "url": "/static/css/main.2abd60b0.chunk.css"
+  },
+  {
+    "revision": "5de23796ca1b0156145d",
+    "url": "/static/js/2.dde1dbc3.chunk.js"
+  },
+  {
+    "revision": "356467dd2cb35a513520",
+    "url": "/static/js/main.72fc1443.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 run-local: export REACT_APP_API_SOURCE=dev
-run-prod: export REACT_APP_API_SOURCE=dev
+run-prod: export REACT_APP_API_SOURCE=pord
 
 run-local:
 	npm start
